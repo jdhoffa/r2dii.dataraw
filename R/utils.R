62405ac70d0995676b4fd644e63b91419d210c8c
Mauro Lepore <maurolepore@gmail.com>
2019-10-15 07:39:46 -0300

    `loanbook_demo` now correctly duplicates IDs (#5)
    * `loanbook_demo` now correctly duplicates IDs
    
    Relates to #4.
    
    In the loanbook data, borrowers or ups with duplicate name and
    sector now have duplicate IDs.
    
    * loanbook.csv:
      * Replaces an outdated file, with a longer name.
      * Was edited to remove a needless column with rownames^[1].
    * Use loanbook.csv as loanbook_demo
    * Update data/loanbook_demo and it's reference for regression
      tests.
    
    [1]: It is easy to overlook that the default is `row.names = TRUE`
         for `utils::write.table()` and friends (e.g.
         `utils::write.csv()`). Prefer functions with safer defaults
         such a `readr::read_csv()` or `vroom::vroom()`.
    
    Thanks @jhoff-2dii.
    
    Signed-off-by: Mauro Lepore <maurolepore@gmail.com>
    
    * New helper remove_rownames_column()
    
    * Move internal remove_rownames_column to utils.R
    
    Allow reuse of remove_loanbook_demo().

--- R/utils.R
+++ R/utils.R
@@ -96,3 +96,19 @@ move_html_to_inst <- function() {
   fs::dir_delete("docs")
 }
 
+#' Helper: If column `X1` exists, remove it
+#'
+#' The column `X1` may exist and contain needless rownames. It's likely the
+#' result of the default argument `row.names = FALSE` to `read.table()`.
+#'
+#' @examples
+#' remove_rownames_column(data.frame(X1 = 1, x = 2))
+#' @noRd
+remove_rownames_column <- function(data) {
+  if (!rlang::has_name(data, "X1")) {
+    return(data)
+  }
+
+  data$X1 <- NULL
+  data
+}
--- data-raw/loanbook_demo.R
+++ data-raw/loanbook_demo.R
@@ -1,8 +1,5 @@
-# Source: Googlesheet provided by Klaus:
-"https://docs.google.com/spreadsheets/d/
-1QHMtoX0j0XMDa33atBPaAYqk__aPQNuIK1v_jsLLd9c/edit#gid=1236522351"
-
-path <- here::here("data-raw/Loanbook - Fake_lbk_SG_based_v4.csv")
-loanbook_demo <- readr::read_csv(path)
+# Source: https://github.com/2DegreesInvesting/r2dii.dataraw/pull/4
+path <- here::here("data-raw/loanbook.csv")
+loanbook_demo <- remove_rownames_column(readr::read_csv(path))
 
 usethis::use_data(loanbook_demo, overwrite = TRUE)
