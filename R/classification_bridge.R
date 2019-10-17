6fd756fdac53ac087ccfc3aac0fb18837952c811
Mauro Lepore <maurolepore@gmail.com>
2019-10-17 14:46:14 -0300

    New data/ to bridge sector classification codes
    Relates to #6.

---
+++ R/classification_bridge.R
@@ -0,0 +1,17 @@
+#' Datasets to translate (bridge) common sector-classification codes
+#'
+#' These datasets help to translate (bridge) common sector classification codes
+#' from the wild to codes we use in 2dii such as 'power', 'oil and gas', 'coal',
+#' 'automotive', 'aviation', 'concrete', 'steel', and 'shipping'.
+#'
+#' @name classification_bridge
+#' @aliases isic_classification nace_classification naics_classification
+#' @seealso [data_dictionary()].
+#'
+#' @return A [tibble::tibble].
+#'
+#' @examples
+#' isic_classification
+#' nace_classification
+#' naics_classification
+NULL
